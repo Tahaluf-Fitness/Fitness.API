8270608ab774e869846b3536632cce1e03e5abb3
halayassen <hmalk7743@gmail.com>
2021-11-22 20:48:16 +0200

    create best food cycle

---
+++ Tahaluf.Fitness/Tahaluf.Fitness.API/Controllers/BestFoodController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tahaluf.Fitness.Core.Data;
+using Tahaluf.Fitness.Core.Service;
+
+namespace Tahaluf.Fitness.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BestFoodController : ControllerBase
+    {
+        private readonly IBestFoodService bestFoodService;
+
+        public BestFoodController(IBestFoodService bestFoodService)
+        {
+            this.bestFoodService = bestFoodService;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(List<BestFood>), StatusCodes.Status200OK)]
+        public List<BestFood> GetBestFood()
+        {
+            return bestFoodService.GetBestFood();
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(BestFood), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public bool CreateBestFood(BestFood bestFood)
+        {
+            return bestFoodService.CreateBestFood(bestFood);
+        }
+
+
+        [HttpPut]
+        [ProducesResponseType(typeof(BestFood), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public bool UpdateBestFood(BestFood bestFood)
+        {
+            return bestFoodService.UpdateBestFood(bestFood);
+        }
+
+        [HttpDelete]
+        [Route("delete/{id}")]
+        [ProducesResponseType(typeof(BestFood), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public bool DeleteBestFood(int id)
+        {
+            return bestFoodService.DeleteBestFood(id);
+        }
+    }
+}
---
+++ Tahaluf.Fitness/Tahaluf.Fitness.Core/Reopsitory/IBestFoodRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Tahaluf.Fitness.Core.Data;
+
+namespace Tahaluf.Fitness.Core.Reopsitory
+{
+    public interface IBestFoodRepository
+    {
+        public List<BestFood> GetBestFood();
+        public bool CreateBestFood(BestFood bestFood);
+        public bool UpdateBestFood(BestFood bestFood);
+        public bool DeleteBestFood(int id);
+    }
+}
